fada445eb1d15939681eb74447a82355ab24b65a
lindseyberlin <linds.berlin@gmail.com>
2022-01-21 19:10:31 -0600

    Another try

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "[theme]
-base = "dark"
+primaryColor="#3872fb"
+backgroundColor="#0e1117"
+secondaryBackgroundColor="#333c4f"
+textColor="#fafafa"
+font="sans serif"
 [server]
 headless = true
 port = $PORT
