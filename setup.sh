b4adbd0eb46ccebbed2cab1ff76e97d120e704eb
lindseyberlin <linds.berlin@gmail.com>
2022-01-24 13:58:19 -0600

    Update setup and remove hidden config files to attempt to change up the styling on the app

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "[theme]
-primaryColor="#3872fb"
-backgroundColor="#0e1117"
-secondaryBackgroundColor="#333c4f"
-textColor="#fafafa"
-font="sans serif"
+primaryColor='#3872fb'
+backgroundColor='#0e1117'
+secondaryBackgroundColor='#333c4f'
+textColor='#fafafa'
+font='sans serif'
 [server]
 headless = true
 port = $PORT
